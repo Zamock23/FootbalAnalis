8bd58e214f5ff415ccc0ca4f070c5711e10ea2ed
Sundukov Artyom <zamock-19@yandex.ru>
2026-06-28 11:16:39 +0300

    Player and his methods

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+type Player struct {
+	Name                   string
+	Goals, Misses, Assists int
+	Rating                 float64
+}
+
+func (player *Player) calculateRating() {
+	var rating float64 = (float64(player.Goals) + float64(player.Assists)) / 2
+	if player.Misses != 0 {
+		rating /= float64(player.Misses)
+	}
+	player.Rating = rating
+}
+
+func NewPlayer(name string, goals, misses, assists int) Player {
+	newPlayer := Player{Name: name, Goals: goals, Misses: misses, Assists: assists}
+	newPlayer.calculateRating()
+	return newPlayer
+}
+
 func main() {
 
 }
